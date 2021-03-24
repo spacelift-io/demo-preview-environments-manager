6a97822462837ac24c824a1c6bfb9e801bf6b8fd
Jacob Martin <jakub.wit.martin@gmail.com>
2021-03-24 16:20:04 +0100

    Automated workflow commit.

--- hello-service/environments/spacelift-io-demo-preview-environments-service-1-4ccad76.tf
+++ hello-service/environments/spacelift-io-demo-preview-environments-service-1-4ccad76.tf
@@ -5,7 +5,7 @@ aamodule "spacelift-io-demo-preview-environments-service-1-4ccad76" {
   infra_repository_branch = "master"
   aws_role                = var.aws_role
   certificate_arn         = var.certificate_arn
-  code_version            = "ea6fb6e44ae44cfc42a691de6e3b7e60d5042b9a"
+  code_version            = "dfb89b5fd616b006da8065a12a4816e83cb377f5"
   domain_name             = var.domain_name
   domain_name_zone_id     = var.domain_name_zone_id
   environment             = "4ccad76"
