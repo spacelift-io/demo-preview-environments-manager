c9cf0f5094a465bd23cf5102a5f9fe3744442066
Jakub Martin <jakub.wit.martin@gmail.com>
2021-03-25 12:49:23 +0100

    Recreate test environment.

---
+++ hello-service/environments/instance_1.tf
@@ -0,0 +1,14 @@
+module "instance_1" {
+  source = "../template"
+
+  manager_stack_id        = var.current_stack_id
+  infra_repository_branch = "master"
+  aws_role                = var.aws_role
+  code_version            = "f541fdef95a91a90e1e0c768f8a9e8ecbc3a22c0"
+  domain_name             = var.domain_name
+  environment             = "ddccbbaa"
+
+  providers = {
+    spacelift = spacelift
+  }
+}
