41390e559e33108020116d37cc6728a3102d7b4a
Jakub Martin <jakub.wit.martin@gmail.com>
2021-03-25 13:06:48 +0100

    Fix

--- hello-service/main.tf
+++ hello-service/main.tf
@@ -35,7 +35,7 @@ resource "aws_route53_record" "endpoint-certificate" {
   }
 
   allow_overwrite = true
-  name            = each.value.nam
+  name            = each.value.name
   records = [
   each.value.record]
   ttl     = 60
@@ -51,16 +51,17 @@ resource "aws_api_gateway_domain_name" "endpoint" {
   security_policy = "TLS_1_2"
 }
 
-//resource "aws_route53_record" "endpoint" {
+resource "aws_route53_record" "endpoint" {
 //  provider = aws.us-east-1
-//  name    = "*.${var.domain_name}"
-//  type    = "A"
-//  zone_id = var.domain_name_zone_id
-//
-//  alias {
-//    evaluate_target_health = true
-//    name                   = aws_api_gateway_domain_name.endpoint.cloudfront_domain_name
-//    zone_id                = aws_api_gateway_domain_name.endpoint.cloudfront_zone_id
-//  }
-//}
+
+  name    = "*.${var.domain_name}"
+  type    = "A"
+  zone_id = var.domain_name_zone_id
+
+  alias {
+    evaluate_target_health = true
+    name                   = aws_api_gateway_domain_name.endpoint.cloudfront_domain_name
+    zone_id                = aws_api_gateway_domain_name.endpoint.cloudfront_zone_id
+  }
+}
 
