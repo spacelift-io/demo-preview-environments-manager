8dbf91d156d4d806ed55ffa746f5b63b96757520
Jakub Martin <jakub.wit.martin@gmail.com>
2021-03-25 13:04:12 +0100

    Fix

--- hello-service/main.tf
+++ hello-service/main.tf
@@ -44,12 +44,15 @@ resource "aws_route53_record" "endpoint-certificate" {
 }
 
 resource "aws_api_gateway_domain_name" "endpoint" {
+  provider = aws.us-east-1
+
   certificate_arn = aws_acm_certificate.endpoint-certificate.arn
   domain_name     = "*.${var.domain_name}"
   security_policy = "TLS_1_2"
 }
 
 //resource "aws_route53_record" "endpoint" {
+//  provider = aws.us-east-1
 //  name    = "*.${var.domain_name}"
 //  type    = "A"
 //  zone_id = var.domain_name_zone_id
