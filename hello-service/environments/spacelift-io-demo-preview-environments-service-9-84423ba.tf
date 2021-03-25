a39a5326aadfb4d05a437a155a177e4fc453ebb5
Jacob Martin <jakub.wit.martin@gmail.com>
2021-03-25 17:47:45 +0100

    Create environment 84423ba

--- hello-service/environments/spacelift-io-demo-preview-environments-service-9-84423ba.tf
+++ hello-service/environments/spacelift-io-demo-preview-environments-service-9-84423ba.tf
@@ -5,7 +5,7 @@ module "spacelift-io-demo-preview-environments-service-9-84423ba" {
   infra_repository_branch = "master"
   aws_role                = var.aws_role
   certificate_arn         = var.certificate_arn
-  code_version            = "781e1cf6e45825c03a811830a428dce449c82b3d"
+  code_version            = "3ff27ccbc9396536d9f7d605a4d42c4791cfa520"
   domain_name             = var.domain_name
   environment             = "84423ba"
   push_policy_id          = var.push_policy_id
