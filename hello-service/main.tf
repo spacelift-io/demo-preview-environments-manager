3fbf8a002c28cc385347ff1a3652179a58ba3e7f
Jakub Martin <jakub.wit.martin@gmail.com>
2021-03-25 12:59:36 +0100

    Remove

--- hello-service/main.tf
+++ hello-service/main.tf
@@ -43,25 +43,25 @@ resource "aws_route53_record" "endpoint-certificate" {
   zone_id = var.domain_name_zone_id
 }
 
-resource "aws_api_gateway_domain_name" "endpoint" {
-  provider = aws.us-east-1
-
-  certificate_arn = aws_acm_certificate.endpoint-certificate.arn
-  domain_name     = "*.${var.domain_name}"
-  security_policy = "TLS_1_2"
-}
-
-resource "aws_route53_record" "endpoint" {
-  provider = aws.us-east-1
-
-  name    = "*.${var.domain_name}"
-  type    = "A"
-  zone_id = var.domain_name_zone_id
-
-  alias {
-    evaluate_target_health = true
-    name                   = aws_api_gateway_domain_name.endpoint.cloudfront_domain_name
-    zone_id                = aws_api_gateway_domain_name.endpoint.cloudfront_zone_id
-  }
-}
+//resource "aws_api_gateway_domain_name" "endpoint" {
+//  provider = aws.us-east-1
+//
+//  certificate_arn = aws_acm_certificate.endpoint-certificate.arn
+//  domain_name     = "*.${var.domain_name}"
+//  security_policy = "TLS_1_2"
+//}
+//
+//resource "aws_route53_record" "endpoint" {
+//  provider = aws.us-east-1
+//
+//  name    = "*.${var.domain_name}"
+//  type    = "A"
+//  zone_id = var.domain_name_zone_id
+//
+//  alias {
+//    evaluate_target_health = true
+//    name                   = aws_api_gateway_domain_name.endpoint.cloudfront_domain_name
+//    zone_id                = aws_api_gateway_domain_name.endpoint.cloudfront_zone_id
+//  }
+//}
 
