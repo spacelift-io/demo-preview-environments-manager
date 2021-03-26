1c76271bdd5eae70f200a96e1552bdef7b771a70
Jacob Martin <jakub.wit.martin@gmail.com>
2021-03-26 11:14:25 +0100

    Create environment 46a5fa6

--- hello-service/environments/spacelift-io-demo-preview-environments-service-11-46a5fa6.tf
+++ hello-service/environments/spacelift-io-demo-preview-environments-service-11-46a5fa6.tf
@@ -5,7 +5,7 @@ module "spacelift-io-demo-preview-environments-service-11-46a5fa6" {
   infra_repository_branch = "master"
   aws_role                = var.aws_role
   certificate_arn         = var.certificate_arn
-  code_version            = "1b79586184cdcf7a28b8c8275e7ffc2ce3eca912"
+  code_version            = "63c45f97f39bbb4e79d6e42761412afb74c54c9c"
   domain_name             = var.domain_name
   environment             = "46a5fa6"
   push_policy_id          = var.push_policy_id
